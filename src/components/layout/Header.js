05cffdd12a10db76acb5bf1d1ee7db360542ba97
Gint0ky <huynguyen90tn@gmail.com>
2024-10-20 12:29:47 +0700

    v1.1 sửa lỗi ESLint errors và cập nhật mới của đăng nhập

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,35 +1,29 @@
 import React from 'react';
-import { Box, Flex, Heading, Button, useColorMode } from '@chakra-ui/react';
-import { FaMoon, FaSun } from 'react-icons/fa';
-import { useDispatch } from 'react-redux';
-import { logout } from '../../store/slices/authSlice';
-import AnimatedBox from '../common/AnimatedBox';
+import { Flex, Heading, Button } from '@chakra-ui/react';
+import { useAuth } from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const dispatch = useDispatch();
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
 
   return (
-    <AnimatedBox
-      initial={{ y: -100 }}
-      animate={{ y: 0 }}
-      exit={{ y: -100 }}
-      transition={{ duration: 0.5 }}
-      as="header"
-      bg="gray.800"
-      color="white"
-      p={4}
-    >
-      <Flex justify="space-between" align="center">
-        <Heading size="lg">TNG Company</Heading>
-        <Flex>
-          <Button onClick={toggleColorMode} mr={4}>
-            {colorMode === 'light' ? <FaMoon /> : <FaSun />}
-          </Button>
-          <Button onClick={() => dispatch(logout())}>Đăng xuất</Button>
-        </Flex>
+    <Flex as="header" align="center" justify="space-between" wrap="wrap" padding="1.5rem" bg="teal.500" color="white">
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg" letterSpacing={'-.1rem'}>
+          TNG Company Management
+        </Heading>
       </Flex>
-    </AnimatedBox>
+
+      <Button onClick={handleLogout} colorScheme="teal" variant="outline">
+        Đăng xuất
+      </Button>
+    </Flex>
   );
 };
 
